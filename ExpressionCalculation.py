3eaffa0b978885e79315721f6c4c85b562c0c111
Anan <anan.kraisakdawat@wsu.edu>
2019-10-30 03:58:45 -0700

    complete test cases for ExpressionCalculation class

--- ExpressionCalculation.py
+++ ExpressionCalculation.py
@@ -17,8 +17,11 @@ class ExpCalc:
             return subExp
 
         try:    # try to directly convert to float value
+            if len(subExp) > 0 and subExp[0] == '~':
+                subExp = '-' + subExp[1:]
             return float(subExp)
         except ValueError:  # if try doesn't work, deal with the expression
+            subExp = self.__negval_split(subExp)
             # prioritize dealing with bracket first
             if ')' in subExp or '(' in subExp:
                 level, maxLevelhead, maxLeveltail = 0, 0, 0
@@ -71,44 +74,72 @@ class ExpCalc:
             else:
                 return "ERROR: invalid expression"
 
-    # helper function to split and caculate
+    # helper function to split and calculate
     def __split_cal(self, subExp, op):
         subExp_split = subExp.split(op)
         res = self.__calculation(subExp_split[0])
         for split in subExp_split[1:]:
-            if op == '*':
-                res *= self.__calculation(split)
+            ret = self.__calculation(split)
+            if isinstance(ret, str) and 'ERR' in ret:
+                return ret
+            if op == '+':
+                res += ret
+            elif op == '-':
+                res -= ret
+            elif op == '*':
+                res *= ret
             elif op == '/':
                 try:
-                    res /= self.__calculation(split)
+                    res /= ret
                 except ZeroDivisionError:
                     return "ERROR: division by 0"
-            elif op == '+':
-                res += self.__calculation(split)
-            elif op == '-':
-                res -= self.__calculation(split)
             elif op == '%':
-                res %= self.__calculation(split)
+                try:
+                    res %= ret
+                except ZeroDivisionError:
+                    return "ERROR: modular by 0"
             elif op == '^':
-                res = pow(res, self.__calculation(split))
+                if res == 0 and ret <= 0:
+                    return "ERROR: 0 cannot be raise to 0 or negative power"
+                res = pow(res, ret)
             elif op == 'root':
-                res = self.__calculation(split) ** (1/res)
+                if res == 0 and ret != 0:
+                    return "ERROR: 0 cannot root anything but 0"
+                elif res < 0 and ret == 0:
+                    return "ERROR: negative number cannot root 0"
+                elif res == 0 and ret == 0:
+                    res = 0
+                else:
+                    res = ret ** (1/res)
             elif op == 'sin':
                 if subExp_split[0] != "":
                     return "ERROR: invalid expression before sin() -> use 3*sin(5) not 3sin(5)"
-                res = math.sin(self.__calculation(split))
+                res = math.sin(ret)
             elif op == 'cos':
                 if subExp_split[0] != "":
                     return "ERROR: invalid expression before cos() -> use 3*cos(5) not 3cos(5)"
-                res = math.cos(self.__calculation(split))
+                res = math.cos(ret)
             elif op == 'tan':
                 if subExp_split[0] != "":
                     return "ERROR: invalid expression before tan() -> use 3*tan(5) not 3tan(5)"
-                res = math.tan(self.__calculation(split))
+                res = math.tan(ret)
         return res
+
+    # helper to determine if '-' is negative or subtract, then deal with it appropriately
+    def __negval_split(self, subExp):
+        newExp = ""
+        for i in range(0, len(subExp)):
+            if subExp[i] == '-':
+                if i == 0 or not subExp[i-1].isdigit():
+                    newExp += '~'
+                    continue
+            newExp += subExp[i]
+
+        return newExp
 
 
 if __name__ == '__main__':
-    expr = "sin(1+sin(1))"
+    expr = '2root-3'
     result = ExpCalc(expr).calculate()
-    print(result,)
+    res = (-3) ** (1/2)
+    print(result, ' ', res)
---
+++ testCalculator.py
@@ -0,0 +1,156 @@
+import unittest
+import math
+from ExpressionCalculation import ExpCalc
+
+
+class ExpressCalculationTestCase(unittest.TestCase):
+    # set up numbers to be tested
+    # also set up test for each operations
+    def setUp(self):
+        self.number_1 = ["-8", "0", "172"]
+        self.number_2 = ["-103", "0", "13"]
+        self.op_for_1 = ["sin", "tan", "cos"]
+        self.op_for_2 = ["+", "-", "*", "/", "%", "^", "root"]
+
+    # test calculate method
+    def test_calculate(self):
+        # test sin
+        for number in self.number_1:
+            expr = "sin" + number
+            self.assertAlmostEqual(math.sin(float(number)), ExpCalc(expr).calculate())
+
+        # test cos
+        for number in self.number_1:
+            expr = "cos" + number
+            self.assertAlmostEqual(math.cos(float(number)), ExpCalc(expr).calculate())
+
+        # test tan
+        for number in self.number_1:
+            expr = "tan" + number
+            self.assertAlmostEqual(math.tan(float(number)), ExpCalc(expr).calculate())
+
+        # test addition
+        add_expected_output = ['-', '-', '-0+', '-', '0', '+', '-0+', '+', '+']
+        index = 0
+        for number1 in self.number_1:
+            for number2 in self.number_2:
+                expr = number1 + "+" + number2
+                res = ExpCalc(expr).calculate()
+                self.assertAlmostEqual(float(number1) + float(number2), res)
+                # check domain
+                res = self.__transformToDomain(res)
+                self.assertIn(res, add_expected_output[index])
+                index += 1
+
+        # test subtraction
+        sub_expected_output = ['-0+', '-', '-', '+', '0', '-', '+', '+', '-0+']
+        index = 0
+        for number1 in self.number_1:
+            for number2 in self.number_2:
+                expr = number1 + "-" + number2
+                res = ExpCalc(expr).calculate()
+                self.assertAlmostEqual(float(number1) - float(number2), res)
+                # check domain
+                res = self.__transformToDomain(res)
+                self.assertIn(res, sub_expected_output[index])
+                index += 1
+
+        # test multiplication
+        mul_expected_output = ['+', '0', '-', '0', '0', '0', '-', '0', '+']
+        index = 0
+        for number1 in self.number_1:
+            for number2 in self.number_2:
+                expr = number1 + "*" + number2
+                res = ExpCalc(expr).calculate()
+                self.assertAlmostEqual(float(number1) * float(number2), res)
+                # check domain
+                res = self.__transformToDomain(res)
+                self.assertIn(res, mul_expected_output[index])
+                index += 1
+
+        # test division
+        div_expected_output = ['+', 'ERR', '-', '0', 'ERR', '0', '-', 'ERR', '+']
+        index = 0
+        for number1 in self.number_1:
+            for number2 in self.number_2:
+                expr = number1 + "/" + number2
+                res = ExpCalc(expr).calculate()
+                if number2 != '0':
+                    self.assertAlmostEqual(float(number1) / float(number2), res)
+                # check domain when not error (type = str when error)
+                if not isinstance(res, str):
+                    res = self.__transformToDomain(res)
+                    self.assertIn(res, div_expected_output[index])
+                else:
+                    self.assertIn(div_expected_output[index], res)
+                index += 1
+
+        # test modular
+        mod_expected_output = ['-0', 'ERR', '0+', '0', 'ERR', '0', '-0', 'ERR', '0+']
+        index = 0
+        for number1 in self.number_1:
+            for number2 in self.number_2:
+                expr = number1 + "%" + number2
+                res = ExpCalc(expr).calculate()
+                if number2 != '0':
+                    self.assertAlmostEqual(float(number1) % float(number2), res)
+                # check domain when not error (type = str when error)
+                if not isinstance(res, str):
+                    res = self.__transformToDomain(res)
+                    self.assertIn(res, mod_expected_output[index])
+                else:
+                    self.assertIn(mod_expected_output[index], res)
+                index += 1
+
+        # test power
+        pow_expected_output = ['-+', '1', '-+', 'ERR', 'ERR', '0', '+', '1', '+']
+        index = 0
+        for number1 in self.number_1:
+            for number2 in self.number_2:
+                expr = number1 + "^" + number2
+                res = ExpCalc(expr).calculate()
+                if number1 != '0' or float(number2) > 0:
+                    self.assertAlmostEqual(float(number1) ** float(number2), res)
+                # check domain when not error (type = str when error)
+                if not isinstance(res, str):
+                    if pow_expected_output[index] == '1':
+                        self.assertAlmostEqual(res, float(pow_expected_output[index]))
+                    else:
+                        res = self.__transformToDomain(res)
+                        self.assertIn(res, pow_expected_output[index])
+                else:
+                    self.assertIn(pow_expected_output[index], res)
+                index += 1
+
+        # test root
+        root_expected_output = ['j', 'ERR', '+', 'ERR', '0', 'ERR', 'j', '0', '+']
+        index = 0
+        for number1 in self.number_1:
+            for number2 in self.number_2:
+                expr = number1 + "root" + number2
+                res = ExpCalc(expr).calculate()
+                if (float(number1) > 0 or number2 != '0') and (number1 != '0' or number2 == '0'):
+                    self.assertAlmostEqual(float(number2) ** (1/float(number1)), res)
+                # check if it's complex number
+                if isinstance(res, complex):
+                    self.assertEqual('j', root_expected_output[index])
+                # or check domain if not error
+                elif not isinstance(res, str):
+                    res = self.__transformToDomain(res)
+                    self.assertIn(res, root_expected_output[index])
+                else:
+                    self.assertIn(root_expected_output[index], res)
+                index += 1
+
+    def __transformToDomain(self, number):
+        if number < 0:
+            return '-'
+        elif number == 0:
+            return '0'
+        elif number > 0:
+            return '+'
+        return self
+
+
+if __name__ == '__main__':
+    unittest.main()
